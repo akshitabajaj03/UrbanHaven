cae00bdbce5fc0127157e1f3c8eab4934427e7cd
akshitabajaj03 <akshitabajaj709@gmail.com>
2023-12-30 15:50:30 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
-import { Router,Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Routes, Navigate} from 'react-router-dom';
+import Success from './pages/Success';
+import { useSelector } from "react-redux";
+
 
 function App() {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element= {<Home/>}/>
+        <Route path="/products/:category" element={<ProductList/>}/>
+        <Route path="/product/:id" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route
+        path="/login"
+        element={user ? <Navigate to="/" /> : <Login />}
+      />
+      <Route
+        path="/register"
+        element={user ? <Navigate to="/" /> : <Register />}
+      />
+        <Route path="/success" element={<Success/>}/>
+      </Routes>
+    </Router>
   );
 }
 
